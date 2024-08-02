97b17fa356eeadf54dd568baa1b935b804e8cc84
Zach Mosher <zmosher4@gmail.com>
2024-08-02 13:12:33 -0500

    added image to welcome page

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,11 +1,11 @@
 import './Welcome.css';
 
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 export const Welcome = () => {
   return (
-    <Container className="text-center mt-5">
+    <Container className="welcome-container text-center mt-5">
       <Row>
         <Col>
           <h1>
@@ -15,6 +15,11 @@ export const Welcome = () => {
           <p className="mt-3">
             A way to keep track of your ever-growing tea collection
           </p>
+          <Image
+            src="https://i0.wp.com/secrettrips.com/wp-content/uploads/2023/08/kerala-tea-fields.jpeg?fit=4824%2C3216&ssl=1"
+            rounded
+            className="mt-4"
+          />
         </Col>
       </Row>
     </Container>
